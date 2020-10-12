d84a6db60a77e07700933c5e0c9886fe4a85ec00
Marvin Neil Sabas <sabasmarvin@yahoo.com>
2020-10-11 21:45:31 -0700

    Created main python file

---
+++ main.py
@@ -0,0 +1,7 @@
+# Main file for Solar Tracking Power Device 
+# Authors: ${Jayrell Quiambao, Roy Matthew Ramos, Marvin Neil Sabas, Michael Sanchez-Hernandez}
+# Last Updated: ${10/11/20}
+
+if __name__ == '__main__':
+    print("Hello World \n") 
+
