e89e22d5e02ea4ac9b27e093777a441e9e7dbad8
Marvin Neil Sabas <sabasmarvin@yahoo.com>
2021-01-08 16:40:41 -0800

    while true dilemma

--- software_merge/jobs.py
+++ software_merge/jobs.py
@@ -3,8 +3,59 @@ import schedule # necessary for scheduling
 # documentation: https://schedule.readthedocs.io/en/stable/api.html
 import time
 from MCP3008 import MCP3008
-from Movement import *
+# from Movement import *
 from multiprocessing import *
+import RPi.GPIO as GPIO
+
+
+OFFSE_DUTY = 0.5        #define pulse offset of servo
+SERVO_MIN_DUTY = 2.5+OFFSE_DUTY     #define pulse duty cycle for minimum angle of servo
+SERVO_MAX_DUTY = 12.5+OFFSE_DUTY    #define pulse duty cycle for maximum angle of servo
+
+servoHPin = 17  #GPIO17 
+servoVPin = 21   #GPI21
+
+servoHAngle = 0
+servoVAngle = 0
+servoHigh = 180 #50 is the adjusted angle
+servoLow = 0
+photoResistor1 = 0 #top left
+photoResistor2 = 1 #top right
+photoResistor3 = 2 #bottom right
+photoResistor4 = 3 #bottom left 
+
+def map( value, fromLow, fromHigh, toLow, toHigh):  # map a value from one range to another range
+    return (toHigh-toLow)*(value-fromLow) / (fromHigh-fromLow) + toLow
+
+def setup():
+    global servoH
+    global servoV
+    #global servoHigh 
+    #global servoLow
+    GPIO.setmode(GPIO.BCM)         # use PHYSICAL GPIO Numbering
+    GPIO.setup(servoHPin, GPIO.OUT)   # Set servoPin to OUTPUT mode
+    GPIO.output(servoHPin, GPIO.LOW)  # Make servoPin output LOW level
+    GPIO.setup(servoVPin, GPIO.OUT)   # Set servoPin to OUTPUT mode
+    GPIO.output(servoVPin, GPIO.LOW)  # Make servoPin output LOW level
+
+    servoH = GPIO.PWM(servoHPin, 50)     # set Frequece to 50Hz
+    servoH.start(0)                     # Set initial Duty Cycle to 0
+    servoV = GPIO.PWM(servoVPin, 50)     # set Frequece to 50Hz
+    servoV.start(0)                     # Set initial Duty Cycle to 0
+
+def servoHWrite(angle):      # make the servo rotate to specific angle 
+    if(angle < servoLow):
+        angle = servoLow
+    elif(angle > servoHigh):
+        angle = servoHigh
+    servoH.ChangeDutyCycle(map(angle,servoLow,servoHigh,SERVO_MIN_DUTY,SERVO_MAX_DUTY)) # map the angle to duty cycle and output it
+    
+def servoVWrite(angle):      # make the servo rotate to specific angle
+    if(angle < servoLow):
+        angle = servoLow
+    elif(angle > servoHigh):
+        angle = servoHigh
+    servoV.ChangeDutyCycle(map(angle,servoLow,servoHigh,SERVO_MIN_DUTY,SERVO_MAX_DUTY)) # map the angle to duty cycle and output it 
 
 def adjust_servo():
     print("Servo Angle Adjustment...")
@@ -19,52 +70,71 @@ def read_photoresistor():
     return [read1, read2, read3, read4]
 
 def move_panel(): 
-    setup()
-    adc_readings = read_photoresistor()
+    global servoHAngle 
+    global servoVAngle 
+    global photoResistor1 
+    global photoResistor2 
+    global photoResistor3 
+    global photoResistor4 
+    global servoHigh 
+    global servoLow
 
-    average12 = (adc_readings[0] + adc_readings[1]) / 2
-    average34 = (adc_readings[2] + adc_readings[3]) / 2
-    average14 = (adc_readings[0] + adc_readings[3]) / 2
-    average23 = (adc_readings[1] + adc_readings[2]) / 2
+    while True:
 
-    if(average12 < average34):
-        if(servoVAngle < servoHigh):
-            servoVWrite(servoVAngle + 1)
-            servoVAngle = servoVAngle + 1
-        else:               
-            servoVAngle = servoHigh
-        time.sleep(0.01)
-    elif(average12 > average34):
-        if(servoVAngle > servoLow):
-            servoVWrite(servoVAngle - 1)
-            servoVAngle = servoVAngle - 1
-        else:
-            servoVAngle = servoLow
-        time.sleep(0.01)
-    else:
-        servoVWrite(servoVAngle)
-        time.sleep(0.01)
+        adc_readings = read_photoresistor()
+
+        print(adc_readings)
+
+        average12 = (adc_readings[0] + adc_readings[1]) / 2
+        average34 = (adc_readings[2] + adc_readings[3]) / 2
+        average14 = (adc_readings[0] + adc_readings[3]) / 2
+        average23 = (adc_readings[1] + adc_readings[2]) / 2
+
         
-    if(average14 > average23):
-        if(servoHAngle < servoHigh):
-            servoHWrite(servoHAngle + 1)
-            servoHAngle = servoHAngle + 1
+        if(average12 < average34):
+            if(servoVAngle < servoHigh):
+                servoVWrite(servoVAngle + 1)
+                servoVAngle = servoVAngle + 1
+            else:               
+                servoVAngle = servoHigh
+            print(f'1st: {servoVAngle}')
+            time.sleep(0.01)
+        elif(average12 > average34):
+            if(servoVAngle > servoLow):
+                servoVWrite(servoVAngle - 1)
+                servoVAngle = servoVAngle - 1
+            else:
+                servoVAngle = servoLow
+            print(f'2nd: {servoVAngle}')
+            time.sleep(0.01)
         else:
-            servoHAngle = servoHigh
-        time.sleep(0.01)
-    elif(average14 < average23):
-        if(servoHAngle > servoLow):
-            servoHWrite(servoHAngle - 1)
-            servoHAngle = servoHAngle - 1
+            servoVWrite(servoVAngle)
+            print(f'1st else: {servoVAngle}')
+            time.sleep(0.01)
+            
+        if(average14 > average23):
+            if(servoHAngle < servoHigh):
+                servoHWrite(servoHAngle + 1)
+                servoHAngle = servoHAngle + 1
+            else:
+                servoHAngle = servoHigh
+            print(f'3rd: {servoVAngle}')
+            time.sleep(0.01)
+        elif(average14 < average23):
+            if(servoHAngle > servoLow):
+                servoHWrite(servoHAngle - 1)
+                servoHAngle = servoHAngle - 1
+            else:
+                servoHAngle = servoLow
+            print(f'4th: {servoVAngle}')
+            time.sleep(0.01)
         else:
-            servoHAngle = servoLow
-        time.sleep(0.01)
-    else:
-        servoVWrite(servoVAngle)
-        time.sleep(0.01)
-    
-    #print('ServoH: ', servoHAngle)
-    print('ServoV: ', servoVAngle)
+            print(f'2nd else: {servoVAngle}')
+            servoVWrite(servoVAngle)
+            time.sleep(0.01)
+        
+        #print('ServoH: ', servoHAngle)
+        print('ServoV: ', servoVAngle)
 
 
 
@@ -83,10 +153,11 @@ def run_sheduled_jobs():
         time.sleep(1)
 
 def movement_schedule():
-    schedule.every(10).seconds.do(move_panel)
+    schedule.every(0.001).seconds.do(move_panel)
     run_sheduled_jobs()
 
 def run_jobs():
+    setup()
     movement_proc = Process(target = movement_schedule)
     movement_proc.start()
     movement_proc.join()
@@ -94,3 +165,7 @@ def run_jobs():
 # developer notes: 
 # need to make sure script can run in parallel 
 # test for which time zone is scheduled (also consider Daylight Saving)
+
+if __name__ == '__main__':
+    setup()
+    move_panel()
--- software_merge/main.py
+++ software_merge/main.py
@@ -1,4 +1,4 @@
-from dbms_connection import *
+# from dbms_connection import *
 from jobs import * 
 
 if __name__ == '__main__':
