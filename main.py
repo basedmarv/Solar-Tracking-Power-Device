aaf496b394b0d272ce05de0aec450516893daa0e
unknown <rmramosvii@gmail.com>
2021-02-26 11:47:24 -0800

    begin the polish process

--- main.py
+++
@@ -1,7 +0,0 @@
-# Main file for Solar Tracking Power Device 
-# Authors: ${Jayrell Quiambao, Roy Matthew Ramos, Marvin Neil Sabas, Michael Sanchez-Hernandez}
-# Last Updated: ${10/11/20}
-
-if __name__ == '__main__':
-    print("Hello World \n") 
-
